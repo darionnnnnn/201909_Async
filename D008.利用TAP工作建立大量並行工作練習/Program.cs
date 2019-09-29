815e28653f9ff5c94d93ed7e52987cb69fe87bd0
Wayne <wayneeeeee@gmail.com>
2019-09-29 15:23:11 +0800

    D008 不只 await，等待的同時還做其他事情

--- D008.利用TAP工作建立大量並行工作練習/Program.cs
+++ D008.利用TAP工作建立大量並行工作練習/Program.cs
@@ -21,33 +21,44 @@ namespace D008.利用TAP工作建立大量並行工作練習
             // HttpClient is Thread-safety 但同時只能發出一個 request，等於多個執行緒互等
             //HttpClient client = new HttpClient();
 
+            var tasks = new Task[10];
+
             for (int i = 0; i < 10; i++)
             {
                 var index = string.Format("{0:D2}", (i + 1));
 
-                Task.Run( async () =>
+                tasks[i] = Task.Run( async () =>
                 {
                     HttpClient client = new HttpClient();
                     // 取得 Thread ID > Thread.CurrentThread.ManagedThreadId
                     // 偵錯時常用到
                     var tid = String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId);
-
-                    var task1 = client.GetStringAsync(url);
+;
                     var task2 = client.GetStringAsync(url);
 
-                    await Task.WhenAll(task1, task2);
 
                     Console.WriteLine($"{index}-1 測試 (TID: {tid}) >>>> {DateTime.Now}");
                     //var result = client.GetStringAsync(url).Result;
-                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) ==== {task1.Result}");
+                    var result = await client.GetStringAsync(url);
+                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) ==== {result}");
                     Console.WriteLine($"{index}-1 測試 (TID: {tid}) <<<< {DateTime.Now}");
 
                     Console.WriteLine($"{index}-2 測試 (TID: {tid}) >>>> {DateTime.Now}");
-                    
-                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) ==== {task2.Result}");
+                    result = await client.GetStringAsync(url);
+                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) ==== {result}");
                     Console.WriteLine($"{index}-2 測試 (TID: {tid}) <<<< {DateTime.Now}");
                 });
 
+            }
+
+
+            var task = Task.WhenAll(tasks);
+
+            while (!task.IsCompleted)
+            {
+                Console.WriteLine("*");
+                // 記得要等 await
+                await Task.Delay(1000);
             }
 
             Console.WriteLine("Press any key to continue...");
@@ -55,3 +66,65 @@ namespace D008.利用TAP工作建立大量並行工作練習
         }
     }
 }
+
+// 保哥版
+//using System;
+//using System.Diagnostics;
+//using System.Net.Http;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace D008.利用TAP工作建立大量並行工作練習
+//{
+//    class Program
+//    {
+//        static async Task Main(string[] args)
+//        {
+//            string host = "https://lobworkshop.azurewebsites.net";
+//            string path = "/api/RemoteSource/Source3";
+//            string url = $"{host}{path}";
+
+//            Stopwatch sw = new Stopwatch();
+
+//            sw.Start();
+
+//            var tasks = new Task[10];
+
+//            for (int i = 0; i < 10; i++)
+//            {
+//                var index = string.Format("{0:D2}", (i + 1));
+
+//                tasks[i] = Task.Run(async () =>
+//                {
+//                    HttpClient client = new HttpClient();
+
+//                    var tid = String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId);
+
+//                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) >>>> {DateTime.Now}");
+//                    var result = await client.GetStringAsync(url);
+//                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) ==== {result}");
+//                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) <<<< {DateTime.Now}");
+
+//                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) >>>> {DateTime.Now}");
+//                    result = await client.GetStringAsync(url);
+//                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) ==== {result}");
+//                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) <<<< {DateTime.Now}");
+//                });
+//            }
+
+//            var task = Task.WhenAll(tasks);
+
+//            while (!task.IsCompleted)
+//            {
+//                Console.WriteLine("*");
+//                await Task.Delay(1000);
+//            }
+
+//            sw.Stop();
+//            Console.WriteLine($"總執行時間 {sw.ElapsedMilliseconds} 毫秒!");
+
+//            Console.WriteLine("Press any key to continue...");
+//            Console.ReadKey();
+//        }
+//    }
+//}
