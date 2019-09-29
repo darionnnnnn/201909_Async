64cf46942ef3890ed7a1523c9754b97626e308d4
Wayne <wayneeeeee@gmail.com>
2019-09-29 16:12:07 +0800

    D008 重構 非同步內容擷取為方法

--- D008.利用TAP工作建立大量並行工作練習/Program.cs
+++ D008.利用TAP工作建立大量並行工作練習/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Net.Http;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,16 +8,14 @@ namespace D008.利用TAP工作建立大量並行工作練習
 {
     class Program
     {
-        //static void Main(string[] args)
+        static string url = "https://lobworkshop.azurewebsites.net/api/RemoteSource/Source3";
+
         static async Task Main(string[] args)
         {
-            string host = "https://lobworkshop.azurewebsites.net";
-            string path = "/api/RemoteSource/Source3";
-            string url = $"{host}{path}";
 
-            // 多執行緒共用一個 HttpClient
-            // HttpClient is Thread-safety 但同時只能發出一個 request，等於多個執行緒互等
-            //HttpClient client = new HttpClient();
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
 
             var tasks = new Task[10];
 
@@ -27,105 +23,41 @@ namespace D008.利用TAP工作建立大量並行工作練習
             {
                 var index = string.Format("{0:D2}", (i + 1));
 
-                tasks[i] = Task.Run( async () =>
+                tasks[i] = Task.Run(async () =>
                 {
-                    HttpClient client = new HttpClient();
-                    // 取得 Thread ID > Thread.CurrentThread.ManagedThreadId
-                    // 偵錯時常用到
-                    var tid = String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId);
-
-                    var task2 = client.GetStringAsync(url);
-
-
-                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) >>>> {DateTime.Now}");
-                    //var result = client.GetStringAsync(url).Result;
-                    var result = await client.GetStringAsync(url);
-                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) ==== {result}");
-                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) <<<< {DateTime.Now}");
-
-                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) >>>> {DateTime.Now}");
-                    result = await client.GetStringAsync(url);
-                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) ==== {result}");
-                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) <<<< {DateTime.Now}");
+                    Task task1 = GetPageAsync(index, 1);
+                    Task task2 = GetPageAsync(index, 2);
+                    await Task.WhenAll(task1, task2);
                 });
-
             }
-
 
             var task = Task.WhenAll(tasks);
 
-            // task.IsCompleted 判斷工作是否完成
             while (!task.IsCompleted)
             {
                 Console.WriteLine("*");
-                // 記得要等 await
                 await Task.Delay(1000);
             }
+
+            sw.Stop();
+            Console.WriteLine($"總執行時間 {sw.ElapsedMilliseconds} 毫秒!");
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
+
+        static async Task<string> GetPageAsync(string index, int num)
+        {
+            HttpClient client = new HttpClient();
+
+            var tid = String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId);
+
+            Console.WriteLine($"{index}-{num} 測試 (TID: {tid}) >>>> {DateTime.Now}");
+            var result = await client.GetStringAsync(url);
+            Console.WriteLine($"{index}-{num} 測試 (TID: {tid}) ==== {result}");
+            Console.WriteLine($"{index}-{num} 測試 (TID: {tid}) <<<< {DateTime.Now}");
+
+            return result;
+        }
     }
 }
-
-// 保哥版
-//using System;
-//using System.Diagnostics;
-//using System.Net.Http;
-//using System.Threading;
-//using System.Threading.Tasks;
-
-//namespace D008.利用TAP工作建立大量並行工作練習
-//{
-//    class Program
-//    {
-//        static async Task Main(string[] args)
-//        {
-//            string host = "https://lobworkshop.azurewebsites.net";
-//            string path = "/api/RemoteSource/Source3";
-//            string url = $"{host}{path}";
-
-//            Stopwatch sw = new Stopwatch();
-
-//            sw.Start();
-
-//            var tasks = new Task[10];
-
-//            for (int i = 0; i < 10; i++)
-//            {
-//                var index = string.Format("{0:D2}", (i + 1));
-
-//                tasks[i] = Task.Run(async () =>
-//                {
-//                    HttpClient client = new HttpClient();
-
-//                    var tid = String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId);
-
-//                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) >>>> {DateTime.Now}");
-//                    var result = await client.GetStringAsync(url);
-//                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) ==== {result}");
-//                    Console.WriteLine($"{index}-1 測試 (TID: {tid}) <<<< {DateTime.Now}");
-
-//                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) >>>> {DateTime.Now}");
-//                    result = await client.GetStringAsync(url);
-//                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) ==== {result}");
-//                    Console.WriteLine($"{index}-2 測試 (TID: {tid}) <<<< {DateTime.Now}");
-//                });
-//            }
-
-//            var task = Task.WhenAll(tasks);
-
-//            while (!task.IsCompleted)
-//            {
-//                Console.WriteLine("*");
-//                await Task.Delay(1000);
-//            }
-
-//            sw.Stop();
-//            Console.WriteLine($"總執行時間 {sw.ElapsedMilliseconds} 毫秒!");
-
-//            Console.WriteLine("Press any key to continue...");
-//            Console.ReadKey();
-//        }
-//    }
-//}
