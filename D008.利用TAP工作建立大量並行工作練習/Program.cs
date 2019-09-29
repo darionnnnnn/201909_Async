897e338e6678583f5ce5281c0db7a0654ad7d126
Wayne <wayneeeeee@gmail.com>
2019-09-29 13:25:07 +0800

    D008 偵錯常用語法

--- D008.利用TAP工作建立大量並行工作練習/Program.cs
+++ D008.利用TAP工作建立大量並行工作練習/Program.cs
@@ -24,6 +24,8 @@ namespace D008.利用TAP工作建立大量並行工作練習
 
                 Task.Run(() =>
                 {
+                    // 取得 Thread ID > Thread.CurrentThread.ManagedThreadId
+                    // 偵錯時常用到
                     var tid = String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId);
 
                     Console.WriteLine($"{index}-1 測試 (TID: {tid}) >>>> {DateTime.Now}");
