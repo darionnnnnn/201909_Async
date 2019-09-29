6d5346d01062b4d8aef197e25d66b2a610bb8c97
Wayne <wayneeeeee@gmail.com>
2019-09-29 11:10:18 +0800

    C007 Thread 改為 Task

--- C007.使用執行緒做到非同步處理作業/Program.cs
+++ C007.使用執行緒做到非同步處理作業/Program.cs
@@ -12,7 +12,33 @@ namespace C007.使用執行緒做到非同步處理作業
         static void Main(string[] args)
         {
             Console.WriteLine($"建立兩個執行緒物件");
-            Thread thread1 = new Thread(() =>
+            //Thread thread1 = new Thread(() =>
+            //{
+            //    Console.WriteLine($"執行緒1 的 ID={Thread.CurrentThread.ManagedThreadId} ( 輸出 X )");
+            //    Thread.Sleep(900);
+            //    for (int i = 0; i < 20; i++)
+            //    {
+            //        Thread.Sleep(100); Console.Write("X");
+            //    }
+            //    //Console.WriteLine();
+            //});
+            //Thread thread2 = new Thread(() =>
+            //{
+            //    Console.WriteLine($"執行緒2 的 ID={Thread.CurrentThread.ManagedThreadId} ( 輸出 - )");
+            //    Thread.Sleep(900);
+            //    for (int i = 0; i < 20; i++)
+            //    {
+            //        Thread.Sleep(150); Console.Write("-");
+            //    }
+            //    //Console.WriteLine();
+            //});
+
+            // Thread 為前景執行緒，改為背景執行緒
+            //thread1.IsBackground = true;
+            //thread2.IsBackground = true;
+
+            // 改為 Task
+            Task task1 = Task.Run(() =>
             {
                 Console.WriteLine($"執行緒1 的 ID={Thread.CurrentThread.ManagedThreadId} ( 輸出 X )");
                 Thread.Sleep(900);
@@ -22,7 +48,7 @@ namespace C007.使用執行緒做到非同步處理作業
                 }
                 //Console.WriteLine();
             });
-            Thread thread2 = new Thread(() =>
+            Task task2 = Task.Run(() =>
             {
                 Console.WriteLine($"執行緒2 的 ID={Thread.CurrentThread.ManagedThreadId} ( 輸出 - )");
                 Thread.Sleep(900);
@@ -33,17 +59,18 @@ namespace C007.使用執行緒做到非同步處理作業
                 //Console.WriteLine();
             });
 
-            // Thread 為前景執行緒，改為背景執行緒
-            //thread1.IsBackground = true;
-            //thread2.IsBackground = true;
 
             Console.WriteLine($"啟動執行兩個執行緒");
 
-            thread1.Start();
-            thread2.Start();
+            //thread1.Start();
+            //thread2.Start();
 
-            thread1.Join(); // 等待 執行緒1 執行完畢
-            thread2.Join(); // 等待 執行緒2 執行完畢
+            //thread1.Join(); // 等待 執行緒1 執行完畢
+            //thread2.Join(); // 等待 執行緒2 執行完畢
+
+            // Task 等待
+            //task1.Wait();
+            //task2.Wait();
 
             Console.WriteLine();
 
