9b95f9ae8364c24edf12d93dc102fa6ae5684470
Wayne <wayneeeeee@gmail.com>
2019-09-29 13:50:21 +0800

    D008 使用執行緒搭配非同步，並等待結果回傳

--- D008.利用TAP工作建立大量並行工作練習/Program.cs
+++ D008.利用TAP工作建立大量並行工作練習/Program.cs
@@ -16,25 +16,38 @@ namespace D008.利用TAP工作建立大量並行工作練習
             string path = "/api/RemoteSource/Source3";
             string url = $"{host}{path}";
 
-            HttpClient client = new HttpClient();
+            // 多執行緒共用一個 HttpClient
+            // HttpClient is Thread-safety 但同時只能發出一個 request，等於多個執行緒互等
+            //HttpClient client = new HttpClient();
 
             for (int i = 0; i < 10; i++)
             {
                 var index = string.Format("{0:D2}", (i + 1));
 
-                Task.Run(() =>
+                Task.Run( async () =>
                 {
+                    HttpClient client = new HttpClient();
                     // 取得 Thread ID > Thread.CurrentThread.ManagedThreadId
                     // 偵錯時常用到
                     var tid = String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId);
 
                     Console.WriteLine($"{index}-1 測試 (TID: {tid}) >>>> {DateTime.Now}");
-                    var result = client.GetStringAsync(url).Result;
+                    //var result = client.GetStringAsync(url).Result;
+                    var result = await client.GetStringAsync(url);
                     Console.WriteLine($"{index}-1 測試 (TID: {tid}) ==== {result}");
                     Console.WriteLine($"{index}-1 測試 (TID: {tid}) <<<< {DateTime.Now}");
+                });
+
+                Task.Run(async () =>
+                {
+                    HttpClient client = new HttpClient();
+                    // 取得 Thread ID > Thread.CurrentThread.ManagedThreadId
+                    // 偵錯時常用到
+                    var tid = String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId);
 
                     Console.WriteLine($"{index}-2 測試 (TID: {tid}) >>>> {DateTime.Now}");
-                    result = client.GetStringAsync(url).Result;
+                    //result = client.GetStringAsync(url).Result;
+                    var result = await client.GetStringAsync(url);
                     Console.WriteLine($"{index}-2 測試 (TID: {tid}) ==== {result}");
                     Console.WriteLine($"{index}-2 測試 (TID: {tid}) <<<< {DateTime.Now}");
                 });
