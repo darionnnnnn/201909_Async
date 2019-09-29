8a8e7d02bc239ef63bd6a48e77efea9fd1f1a23a
Wayne <wayneeeeee@gmail.com>
2019-09-29 16:14:15 +0800

    D008 保哥版 擷取非同步錯誤

--- D008.利用TAP工作建立大量並行工作練習/Program.cs
+++ D008.利用TAP工作建立大量並行工作練習/Program.cs
@@ -23,20 +23,35 @@ namespace D008.利用TAP工作建立大量並行工作練習
             {
                 var index = string.Format("{0:D2}", (i + 1));
 
+                Task task1 = GetPageAsync(index, 1);
+                Task task2 = GetPageAsync(index, 2);
+
                 tasks[i] = Task.Run(async () =>
                 {
-                    Task task1 = GetPageAsync(index, 1);
-                    Task task2 = GetPageAsync(index, 2);
-                    await Task.WhenAll(task1, task2);
+                    try
+                    {
+                        await Task.WhenAll(task1, task2);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine(ex.Message);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
                 });
             }
 
-            var task = Task.WhenAll(tasks);
-
-            while (!task.IsCompleted)
+            try
             {
-                Console.WriteLine("*");
-                await Task.Delay(1000);
+                await Task.WhenAll(tasks);
+            }
+            catch (Exception ex)
+            {
+                throw;
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.BackgroundColor = ConsoleColor.Black;
             }
 
             sw.Stop();
@@ -53,6 +68,12 @@ namespace D008.利用TAP工作建立大量並行工作練習
             var tid = String.Format("{0:D2}", Thread.CurrentThread.ManagedThreadId);
 
             Console.WriteLine($"{index}-{num} 測試 (TID: {tid}) >>>> {DateTime.Now}");
+
+            if (index.IndexOf("5") >= 0)
+            {
+                throw new ArgumentException("5 is invalid");
+            }
+
             var result = await client.GetStringAsync(url);
             Console.WriteLine($"{index}-{num} 測試 (TID: {tid}) ==== {result}");
             Console.WriteLine($"{index}-{num} 測試 (TID: {tid}) <<<< {DateTime.Now}");
